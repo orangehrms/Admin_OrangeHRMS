8fea5b5c22c3516d0266dcb8b5cc54560037df5a
Madhu <orangehrms16@gmail.com>
2016-11-06 16:25:05 +0530

    Initiate Orange HRMS project

---
+++ Orange_hrms/src/test/java/com/orange_hrms/Welcome.java
@@ -0,0 +1,28 @@
+package com.orange_hrms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class Welcome {
+	WebDriver driver = null;
+	
+  @Test
+  public void googlehome() {
+	  driver.get("http://www.google.com");
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.manage().deleteAllCookies();
+	  driver.quit();
+  }
+
+}
