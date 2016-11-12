72515763cd3325f2ad178095c957694a8d3ed3f7
Madhu <orangehrms16@gmail.com>
2016-11-12 23:25:39 +0530

    add Log4j TestNG Listener

--- Orange_hrms/src/test/java/com/orange_hrms/Welcome.java
+++ Orange_hrms/src/test/java/com/orange_hrms/Welcome.java
@@ -1,28 +1,37 @@
 package com.orange_hrms;
 
 import org.testng.annotations.Test;
+
+import com.orange_hrms.utils.MultiBrowser;
+
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 
 public class Welcome {
+	
 	WebDriver driver = null;
+	//static String Firefox=null;
 	
   @Test
   public void googlehome() {
-	  driver.get("http://www.google.com");
+	  
+	  driver.get("http:\\www.google.com");
   }
   
   @BeforeTest
   public void beforeTest() {
+	  
 	  driver = new FirefoxDriver();
+	  
+	//  MultiBrowser.Browser(Firefox);
   }
 
   @AfterTest
   public void afterTest() {
-	  driver.manage().deleteAllCookies();
-	  driver.quit();
+	 driver.manage().deleteAllCookies();
+	 driver.quit();
   }
 
 }
